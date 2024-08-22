a575f9ca2d3a040c8fe2b4f8c689239f13091236
bruvio <bruno.viola@genomicsengland.co.uk>
2024-08-22 06:48:58 +0200

    feat: adding s3 bucket as storage and use to sync data to nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,11 @@ resource "aws_key_pair" "deployer" {
   public_key = file("${path.module}/deployer_key.pub")
 }
 
+resource "aws_iam_instance_profile" "example" {
+  name = "example-instance-profile"
+  role = aws_iam_role.example.name
+}
+
 resource "aws_instance" "example" {
   count           = 3
   ami             = "ami-08aa7f71c822e5cc9" # Ubuntu AMI
@@ -105,7 +110,7 @@ resource "aws_instance" "example" {
   root_block_device {
     volume_size = 20
   }
-
+  iam_instance_profile = aws_iam_instance_profile.example.name
   tags = {
     Name = "${count.index == 0 ? "master" : "worker-${count.index}"}"
   }
@@ -168,7 +173,9 @@ resource "aws_instance" "example" {
     sleep 15
 
     echo "installing calico"
+    sudo su
     export KUBECONFIG=/etc/kubernetes/admin.conf
+    exit
 
     # Step 6: Install Network Plugin on the Master (Calico)
     sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/tigera-operator.yaml 
@@ -179,6 +186,7 @@ resource "aws_instance" "example" {
         # Output the join command for the worker nodes
     sudo kubeadm token create --print-join-command > /home/ubuntu/kubeadm_join_command.sh
     sudo chmod +x /home/ubuntu/kubeadm_join_command.sh
+    aws s3 cp /home/ubuntu/kubeadm_join_command.sh s3://${random_pet.bucket_name.id}/kubeadm_join_command.sh
 
     echo "installing ks9 dashboard"
     curl -sS https://webinstall.dev/k9s | bash
@@ -194,12 +202,12 @@ resource "aws_instance" "example" {
 
     helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 
+    aws s3 cp  s3://${random_pet.bucket_name.id}/ /home/ubuntu/ --recursive
 
     else
         sudo systemctl enable kubelet
-        sudo mkdir -p $HOME/.kube
-        sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-        sudo chown $(id -u):$(id -g) $HOME/.kube/config
+        aws s3 cp  s3://${random_pet.bucket_name.id}/kubeadm_join_command.sh /home/ubuntu/kubeadm_join_command.sh
+        sudo ./kubeadm_join_command.sh
     fi
   EOF
 }
@@ -207,49 +215,123 @@ resource "aws_instance" "example" {
 output "instance_public_ips" {
   value = aws_instance.example[*].public_ip
 }
+# Generate a random bucket name
+resource "random_pet" "bucket_name" {
+  length = 2
+}
+resource "aws_s3_bucket" "this" {
+  bucket        = random_pet.bucket_name.id
+  force_destroy = true
+}
 
 
-locals {
-  folders = {
-    "folder1" = {
-      source      = "../simple/"
-      destination = "/home/ubuntu/simple/"
-    }
-    "folder2" = {
-      source      = "../helloHttpd/"
-      destination = "/home/ubuntu/helloHttpd/"
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.bucket
 
-# Null resource to handle local folder copy
-resource "null_resource" "copy_folder" {
-  depends_on = [aws_instance.example[0]]
-  for_each = local.folders
-  provisioner "file" {
-    source      = each.value.source
-    destination = each.value.destination
-
-    connection {
-      type        = "ssh"
-      host        = aws_instance.example[0].public_ip
-      user        = "ubuntu"  # 
-      private_key = file("./deployer_key")  #
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "ls -la ${each.value.destination}",
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = aws_instance.example[0].public_ip
-      user        = "ubuntu"
-      private_key = file("./deployer_key")
-    }
-  }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
+output "bucket_id" {
+  value = aws_s3_bucket.this.id
+}
+# Upload files recursively from both folders
+resource "aws_s3_bucket_object" "folder1" {
+  for_each = fileset(var.folder1_path, "**/*")
+
+  bucket = aws_s3_bucket.this.id
+  key    = each.key # Corrected: Use only the relative path inside the folder
+  source = "${var.folder1_path}/${each.key}"
+  acl    = "private"
+  etag   = filemd5("${var.folder1_path}/${each.key}")
+}
+
+resource "aws_s3_bucket_object" "folder2" {
+  for_each = fileset(var.folder2_path, "**/*")
+
+  bucket = aws_s3_bucket.this.id
+  key    = each.key # Corrected: Use only the relative path inside the folder
+  source = "${var.folder2_path}/${each.key}"
+  acl    = "private"
+  etag   = filemd5("${var.folder2_path}/${each.key}")
+}
+
+resource "aws_s3_bucket_object" "setup" {
+
+
+  bucket = aws_s3_bucket.this.id
+  key    = "setup_ks8.sh"   # The destination path in S3
+  source = "./setup_ks8.sh" # The path to the file on your local machine
+
+  acl  = "private"
+  etag = filemd5("./setup_ks8.sh")
+}
+
+variable "folder1_path" {
+  type    = string
+  default = "../simple"
+}
+
+variable "folder2_path" {
+  type    = string
+  default = "../helloHttpd"
+}
+variable "versioning" {
+  type    = bool
+  default = false
+}
+
+
+resource "aws_iam_role" "example" {
+  name = "ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "example" {
+  name = "example-policy"
+  role = aws_iam_role.example.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ],
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
